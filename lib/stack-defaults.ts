d46c497cd8f8cdd1df70399627f370e30b8e3f52
Kevin Elliott <skiimilk@gmail.com>
2022-06-22 14:13:12 +0800

    Use reservedConcurrentExecutions on lambda functions

--- lib/stack-defaults.ts
+++ lib/stack-defaults.ts
@@ -22,8 +22,19 @@ function getDefaultLambdaProps(): Partial<NodejsFunctionProps> {
     },
     handler: "main",
     memorySize: 128,
-    // TODO: Bring back when concurrent executions quota has been sorted out in my account.
-    // reservedConcurrentExecutions: 1,
+    // Try to ensure mistakes in lambda function configurations don't lead to large lambda runtime costs by restricting
+    // the number of current executions of any given function to 1.
+    //
+    // NOTE: In order to use this option, your "ConcurrentExecutions" account limit needs to be larger than 100, because
+    //       the minimum "UnreservedConcurrentExecutions" is 100 and reserved concurrency is taken from the account's
+    //       total "ConcurrentExecutions". For example, if your "ConcurrentExecutions" account limit is 101, then you
+    //       will be able to use a `reservedConcurrentExecutions` of 1 on one lambda function.
+    //       For more details, see:
+    //         - https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-limits.html
+    //         - https://docs.aws.amazon.com/lambda/latest/dg/configuration-concurrency.html
+    //         - https://docs.aws.amazon.com/lambda/latest/dg/invocation-scaling.html
+    //         - https://docs.aws.amazon.com/servicequotas/latest/userguide/request-quota-increase.html
+    reservedConcurrentExecutions: 1,
     retryAttempts: 0,
     runtime: lambda.Runtime.NODEJS_16_X,
     timeout: cdk.Duration.seconds(DEFAULT_LAMBDA_TIMEOUT_SECONDS)
